7331a8acaa82ec013350403109eba0469aa7337f
Dev Random <c1.devrandom@niftybox.net>
2013-09-13 11:27:26 -0700

    JSON history command

--- lib/commands.py
+++ lib/commands.py
@@ -256,17 +256,7 @@ class Commands:
         balance = 0
         out = []
         for item in self.wallet.get_tx_history():
-            tx_hash, conf, is_mine, value, fee, balance, timestamp = item
-            try:
-                time_str = datetime.datetime.fromtimestamp( timestamp).isoformat(' ')[:-3]
-            except:
-                time_str = "----"
-
-            label, is_default_label = self.wallet.get_label(tx_hash)
-            if not label: label = tx_hash
-            else: label = label + ' '*(64 - len(label) )
-
-            out.append( "%16s"%time_str + "  " + label + "  " + format_satoshis(value)+ "  "+ format_satoshis(balance) )
+            out.append(item)
         return out
 
 
