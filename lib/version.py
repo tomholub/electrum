a49cdb5fe3239b092b6fc8cfc12e20eb523f59b0
Devrandom <c1.devrandom@niftybox.net>
2014-03-01 14:43:50 -0800

    Bump version

--- lib/version.py
+++ lib/version.py
@@ -1,4 +1,4 @@
-ELECTRUM_VERSION = "2.0-cc-002"    # version of the client package
+ELECTRUM_VERSION = "2.0-cc-003"    # version of the client package
 PROTOCOL_VERSION = '0.9'    # protocol version requested
 SEED_VERSION     = 6        # bump this every time the seed generation is modified
 SEED_PREFIX      = '01'     # the hash of the mnemonic seed must begin with this
